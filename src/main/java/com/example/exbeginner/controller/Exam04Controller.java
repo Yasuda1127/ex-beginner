f240ef1f2e6c06005581f45dd7b205f3d96572bd
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2024-02-06 15:13:25 +0900

    exam04 complete

---
+++ src/main/java/com/example/exbeginner/controller/Exam04Controller.java
@@ -0,0 +1,37 @@
+package com.example.exbeginner.controller;
+
+import com.example.exbeginner.model.User;
+
+import jakarta.validation.Valid;
+
+import com.example.exbeginner.form.UserForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Exam04Controller {
+
+    @RequestMapping("/exam04")
+    public String start(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "exam04";
+    }
+
+    @PostMapping("/exam04-result")
+    public String result(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "exam04"; // エラーがある場合は入力画面に戻る
+        }
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setAge(userForm.getAge());
+        user.setComment(userForm.getComment());
+
+        model.addAttribute("user", user);
+        return "exam04-result";
+    }
+}
